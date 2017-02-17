1a13d531b2010fd8dc92e9d62dab58a44abd020d
sirjuan <t6peju01@students.oamk.fi>
2017-02-17 18:31:08 +0200

    developing email send

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
 app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.use(express.static("dist")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(app.get('port'), function () {
     console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port: ", app.get('port'));
