f25765f6725dbb87f575b7d41c156587cac9fe28
sirjuan <t6peju01@students.oamk.fi>
2017-02-17 13:49:19 +0200

    developing email send

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var api_key = 'key-2cd3537191d3a31aac7bc257dc32713d';
 var domain = 'https://api.mailgun.net/v3/https://warm-sands-84114.herokuapp.com';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
 var app = express();
 
 
@@ -15,11 +13,11 @@ app.listen(app.get('port'), function () {
     console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port: ", app.get('port'));
 });
 
-app.post("/api/post", function(req, res) {
+app.get("/api/post/:email", function(req, res) {
 
     var data = {
         from: 'Liana Technologies <newsletter@lianatech.com>',
-        to: email,
+        to: req.params.email,
         subject: 'Your subscription',
         text: 'Thank you for subscribing to Liana Technologies newsletter!'
     };
