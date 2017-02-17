0d9d6dd70318f4313a8f1ca564e8e76800416f2e
sirjuan <t6peju01@students.oamk.fi>
2017-02-17 14:10:28 +0200

    developing email send

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var api_key = 'key-2cd3537191d3a31aac7bc257dc32713d';
-var domain = 'warm-sands-84114.herokuapp.com';
+var domain = 'https://warm-sands-84114.herokuapp.com';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 var express = require('express');
 var app = express();
